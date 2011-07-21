67806f2f877fdfd43784ba055783549805da3e04
eweitnau <eweitnauer@gmail.com>
2011-07-21 13:22:43 +0200

    added phiclient, a palm app that will be a testing and performancing app for the socket.io communication

---
+++ phone/palm/phiclient/app/assistants/main-assistant.js
@@ -0,0 +1,130 @@
+function MainAssistant() {
+}
+
+MainAssistant.prototype.create_pressed = function(evt) {
+
+}
+
+JoinDialogAssistant = Class.create({
+	initialize: function(sceneAssistant) {
+		this.sceneAssistant = sceneAssistant;
+		this.controller = sceneAssistant.controller;
+	},
+	
+	setup : function(widget) {
+		this.widget = widget;
+		this.controller.setupWidget("session_code", { textCase: Mojo.Widget.steModeLowerCase, hintText: '... type here' }, { });
+		this.controller.get('join_button').addEventListener(Mojo.Event.tap, this.join_tapped.bindAsEventListener(this));
+		this.controller.get('cancel_button').addEventListener(Mojo.Event.tap, this.cancel_tapped.bindAsEventListener(this));
+	},
+	
+	cancel_tapped: function() {
+		this.widget.mojo.close();
+	},
+	
+	join_tapped: function() {
+	  this.widget.mojo.close();
+	  this.sceneAssistant.join_session(this.controller.get('session_code').mojo.getValue());
+	}
+});
+
+MainAssistant.prototype.join_session = function(code) {
+  Mojo.Log.info('Joining session ' + code + '...');
+}
+
+MainAssistant.prototype.join_pressed = function(evt) {
+  Mojo.Log.info('Join pressed.');
+  this.controller.showDialog({
+	  template: 'main/join-dialog',
+		assistant: new JoinDialogAssistant(this)
+	});
+}
+
+MainAssistant.prototype.on_connect = function(item) {
+  item.label = 'CONN';
+  item.label_class = '';
+  item.spinning = false;
+  this.controller.modelChanged(this.connectionsModel, this);
+  Mojo.Log.info('Connected to ' + item.addr + '!');
+  if (item.timerId) {
+    clearTimeout(item.timerId);
+    item.timerId = null;
+  }
+}
+
+MainAssistant.prototype.on_disconnect = function(item) {
+  item.label = '';
+  item.label_class = '';
+  item.spinning = false;
+  this.controller.modelChanged(this.connectionsModel, this);
+  Mojo.Log.info('Disconnected from ' + item.addr + '!');
+  if (item.timerId) {
+    clearTimeout(item.timerId);
+    item.timerId = null;
+  }
+}
+
+MainAssistant.prototype.on_conn_err = function(item) {
+  item.label = 'ERROR';
+  item.label_class = 'red';
+  item.spinning = false;
+  this.controller.modelChanged(this.connectionsModel, this);
+  Mojo.Log.info('Could not connect / disconnect from ' + item.addr + '!');
+}
+
+MainAssistant.prototype.connection_list_tapped = function(evt) {
+  if (evt.item.spinning) return;
+  evt.item.spinning = true;
+  evt.item.timerId = setTimeout(this.on_conn_err.bind(this, evt.item), 5000);
+  this.controller.modelChanged(this.connectionsModel, this); 
+  if (typeof(evt.item.socket) == 'undefined') {
+    evt.item.socket = io.connect(evt.item.addr);
+    evt.item.socket.on('connect', this.on_connect.bind(this, evt.item));
+    evt.item.socket.on('disconnect', this.on_disconnect.bind(this, evt.item));
+  } else if (evt.item.socket.socket.connected) {
+    evt.item.socket.socket.disconnect();
+  } else {
+    evt.item.socket.socket.connect();
+  }
+}
+
+MainAssistant.prototype.setup = function() {
+//  this.controller.setupWidget("btnCreate", {}, {label: "Create", disabled: false});
+//  Mojo.Event.listen(this.controller.get("btnCreate"),
+//                    Mojo.Event.tap,
+//                    this.create_pressed.bindAsEventListener(this));
+
+  this.controller.setupWidget("btnJoin", {}, {label: "Join Session", disabled: false});
+  Mojo.Event.listen(this.controller.get("btnJoin"),
+                    Mojo.Event.tap,
+                    this.join_pressed.bindAsEventListener(this));
+
+  this.controller.setupWidget("connection-list",
+      this.connectionsAttr = {
+        itemTemplate: "main/item-template"
+      },
+      this.connectionsModel = {
+        listTitle: "connections",
+        items: [{addr: 'http://192.168.0.102:9888', label: '', spinning: false},
+                {addr: 'http://phigames.com:9888', label: '', spinning: false}]
+      }
+  );
+  this.controller.listen(this.controller.get("connection-list"),
+                         Mojo.Event.listTap,
+                         this.connection_list_tapped.bindAsEventListener(this));
+};
+
+MainAssistant.prototype.activate = function(event) {
+	/* put in event handlers here that should only be in effect when this scene is active. For
+	   example, key handlers that are observing the document */
+};
+
+MainAssistant.prototype.deactivate = function(event) {
+	/* remove any event handlers you added in activate and do any other cleanup that should happen before
+	   this scene is popped or another scene is pushed on top */
+};
+
+MainAssistant.prototype.cleanup = function(event) {
+	/* this function should do any cleanup needed before the scene is destroyed as 
+	   a result of being popped off the scene stack */
+};
