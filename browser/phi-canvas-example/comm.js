dc0b566c14ddcdaa2cff268ba71611dca9a9f594
Erik Weitnauer <eweitnauer@gmail.com>
2011-07-27 10:03:36 +0200

    added example of how to use phi server comm. with canvas drawing in a pong game.

---
+++ browser/phi-canvas-example/comm.js
@@ -0,0 +1,31 @@
+var socket, session_code;
+
+function log() {
+  var msg = [];
+  for (i in arguments) msg.push(arguments[i]);
+  msg = msg.join(' ');
+  console.log(msg);
+  var p = document.getElementById('log_text');
+  p.appendChild(document.createElement('br'));
+  p.appendChild(document.createTextNode(msg));
+};
+
+function comm_init() {
+  socket = io.connect('http://phigames.com:9888');
+  
+  socket.on('connect', function() {
+    log('connected');
+  });
+  socket.on('disconnect', function() {
+    log('disconnected');
+  });  
+  socket.on('player_joined', function(code) {
+    log('player joined session', code);
+  });
+}
+
+function create_session() {
+  socket.emit('create_session', function(code, success) {
+    document.getElementById('session-code').innerHTML = success ? code : 'fail';
+  });
+}
