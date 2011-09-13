dff46ed9e43f29e855c7044d22ab26f20e74cf11
eweitnau <eweitnauer@gmail.com>
2011-09-13 12:13:56 +0200

    working on mobile webpage

--- shooty/phone/webpage/controller.js
+++ shooty/phone/webpage/controller.js
@@ -4,15 +4,40 @@ function init() {
   var params = getUrlParameters();
   console.log('code: ', params.session_code);
   console.log('name: ', params.player_name);
-  comm = new Communicator(io);
-  comm.connect();
-  comm.on('connect', function() {
-    comm.join_session(params.session_code, {player_name: params.player_name}, function(code) {
-      console.log('Joined Successfully!');
-    });
-  });
+//  comm = new Communicator(io);
+//  comm.connect();
+//  comm.on('connect', function() {
+//    comm.join_session(params.session_code, {player_name: params.player_name}, function(code) {
+//      document.getElementById('conn_status').innerHTML = 'Connected!';
+//    });
+//  });
+  setupListeners();
 }
 
+function setupListeners() {
+  var label1 = document.getElementById('label1');
+  var label2 = document.getElementById('label2');
+  var label3 = document.getElementById('label3');
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(event) {
+      label1.innerHTML = event.alpha + ", " + event.beta + ', ' + event.gamma;
+    }, false);
+  } else if (window.OrientationEvent) {
+    window.addEventListener('MozOrientation', function(event) {
+      label2.innerHTML = event.x + ", " + event.y + ", " + event.z;
+    }, false);
+  } else {
+    var touchListener = function(event) {
+      for (var i = 0; i < event.touches.length; i++) {
+        var touch = event.touches[i];
+        console.log(touch);
+      }
+    }
+    document.getElementById('bg').addEventListener('touchmove', touchListener, false);
+    document.getElementById('bg').addEventListener('touchstart', touchListener, false);
+  }
+}
+ 
 function getUrlParameters() {
   var map = {};
   var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
