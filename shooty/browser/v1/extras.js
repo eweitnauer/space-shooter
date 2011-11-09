c985d04b8c006fdd90ca358ad14dd56b39f21e46
Christof Elbrechter <celbrech@xt2.(none)>
2011-11-09 11:47:35 +0100

    added extras

---
+++ shooty/browser/v1/extras.js
@@ -0,0 +1,26 @@
+var Extras = function(){
+    this.names = [ 'health','acceleration','bullet-speed','life','shield',
+                   'shot','shot-angle','shot-length','shot-steangth' ];
+    this.levels = {}
+    this.costs = {}
+    this._costs_values_raw = [
+        [ 10, 20, 40, 80, 160 ],
+        [ 100, 200, 400, 800, 1000],
+        [ 200, 400, 800, 1600, 5000],
+        [ 500, 500, 1000, 2000, 10000],
+        [ 100, 200, 500, 1000, 5000],
+        [ 1000, 2000, 5000, 10000, 20000],
+        [ 100, 200, 400, 400, 400],
+        [ 400, 800, 1600, 5000, 10000],
+        [ 1000, 2000, 5000, 10000, 20000]
+    ];
+
+    this.init = function(){
+        for(var i=0;i<this.names.length;i++){
+            this.levels[this.names[i]] = 0;
+            this.costs[this.names[i]] = this._costs_values_raw[i];
+        }
+    }
+    
+    this.init();
+};
