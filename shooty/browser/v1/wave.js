7ec7c9768f3d5489c8ad2fdf355a85333cad422e
Christof Elbrechter <celbrech@techfak.uni-bielefeld.de>
2011-12-13 10:00:48 +0100

    desperate checkin before new branch

--- shooty/browser/v1/wave.js
+++ shooty/browser/v1/wave.js
@@ -7,4 +7,35 @@ var Wave = function(level, timeSeconds, listOfSubWaves){
   this.timeSeconds = timeSeconds;
   this.listOfSubWaves;
   this.create_splash_screen = null;
+  this.startTime = null;
 }
+
+Wave.prototye.start = function(){
+  this.startTime = Animation.time;
+  var dt = (Animation.time - this.startTime)/1000;
+  while( a.length && (a[0].timeOffset < dt) ){
+    this.spawn_sub_save(a[0]);
+    a.shift();
+  }
+  if(!a.length) clearInterval
+}
+
+
+
+t0 = Date.now();
+handle = setInterval(checkTimeline, 100);
+
+console.log('hi there!')
+
+function checkTimeline(t) {
+  dt = Date.now()-t0;
+  console.log('dt',dt)
+  if (a[0][0] < dt/1000) {
+    curr = a.shift();
+    console.log(curr[1]);
+    if (a.length==0) clearInterval(handle);
+  }
+}
+
+
+
